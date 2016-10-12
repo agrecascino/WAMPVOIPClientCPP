6bc57486df93a84f349e303e1c2ff5c7123e7295
agrecascino <agrecascino123@gmail,com>
2016-10-11 23:34:06 -0400

    It all "Just Werks"!

--- WAMPVOIPClientCPP.pro
+++ WAMPVOIPClientCPP.pro
@@ -9,6 +9,6 @@ HEADERS += \
     includes.h
 
 DEFINES += LTM_DESC
-unix|win32: LIBS += -lboost_system -lboost_thread -lssl -lcrypto -lpthread -ltfm -ltomcrypt -ltommath
+unix|win32: LIBS += -lboost_system -lboost_thread -lssl -lopus -lportaudio -lcrypto -lpthread -ltfm -ltomcrypt -ltommath
 
 QMAKE_CXXFLAGS += -std=c++14 -O0 -g3 -fstack-protector-all
--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include "includes.h"
 using namespace std;
+struct RemoteUser {
+    RemoteUser(string name_temp,autobahn::wamp_subscription subscription_temp)
+    {
+        name = name_temp;
+        subscription = subscription_temp;
+
+    }
+
+    string name;
+    autobahn::wamp_subscription subscription;
+};
+
 struct User {
     string name;
     string channel;
+    vector<RemoteUser> channelusers;
     vector<unsigned char> pubkey;
     vector<unsigned char> privkey;
 };
 User current_user;
+OpusEncoder *encoder;
+OpusDecoder *decoder;
+PaStream *stream;
 boost::asio::io_service io;
 auto session = make_shared<autobahn::wamp_session>(io,false);
 void publish_to_channel(string channame,vector<string> arguments)
@@ -14,10 +30,11 @@ void publish_to_channel(string channame,vector<string> arguments)
     vector<string> base64_encrypted_arguments;
     for(string arg : arguments)
     {
-        unsigned char *encrypted_out = new unsigned char[1024];
+        unsigned char *encrypted_out = new unsigned char[512];
 
-        unsigned long outlen = 1024;
-        if(rsa_encrypt_key_ex((unsigned char*)(void*)arg.c_str(),arg.size(),encrypted_out,&outlen,NULL,NULL,NULL,0,0,LTC_LTC_PKCS_1_V1_5,&serv_pub) != CRYPT_OK)
+        unsigned long outlen = 512;
+
+        if(rsa_encrypt_key_ex((unsigned char*)(void*)arg.c_str(),arg.size(),encrypted_out,&outlen,NULL,NULL,NULL,0,0,LTC_PKCS_1_V1_5,&serv_pub) != CRYPT_OK)
         {
             cout << "Encryption failed" << endl;
         }
@@ -37,27 +54,50 @@ void infinite_ping_loop()
     }
 }
 thread *t;
+thread *t2;
+void audio_encode()
+{
+     while(true)
+     {
+     short data[512];
+     Pa_ReadStream(stream,&data,512);
+     unsigned char packet[3*1276];
+     int nbBytes = opus_encode(encoder,data,512,packet,3*1276);
+     vector<unsigned char> packt(packet,packet + 512);
+     session->publish("com.audiodata." + current_user.name,packt);
+     }
+}
+
+void audio_play(const autobahn::wamp_event& event)
+{
+    vector<unsigned char> packet;
+    try{
+        packet = event.argument<vector<unsigned char>>(0);
+
+    }catch(const std::exception &e)
+    {
+        cout << e.what() << endl;
+    }
+    short output[3*1276];
+    int frame_size = opus_decode(decoder,packet.data(),packet.size(),output,3*1276,0);
+    Pa_WriteStream(stream,&output,frame_size);
+}
 void process_command(const autobahn::wamp_event& event)
 {
 
     vector<vector<string>> arguments;
-    cout << event.number_of_arguments() << endl;
        for(unsigned int i = 0;i < event.number_of_arguments();i++)
        {
            try{
-               arguments.push_back(event.argument<vector<string>>(i));
+               arguments.push_back(event.argument<vector<string>>(0));
 
            }catch(const std::exception &e)
            {
-
                cout << e.what() << endl;
-               raise(SIGINT);
-               exit(-1);
            }
 
        }
            if(arguments[0][0] == "~"){
-
 
                if(arguments[0][1] == "PUBKEY"){
                    string base64publickey = arguments[0][2];
@@ -66,22 +106,50 @@ void process_command(const autobahn::wamp_event& event)
                    base64_decode((unsigned char*)(void*)base64publickey.c_str(),base64publickey.size(),nonb64key,&k64len);
                    rsa_import(nonb64key,k64len,&serv_pub);
                    cout << base64publickey << endl;
+                   t2 = new thread(audio_encode);
                    t = new thread(infinite_ping_loop);
                    t->detach();
                }
            }
                else{
                    for(int i = 0;i < arguments[0].size();i++){
-                       string argument = base_64_encode(arguments[0][i]);
+                       string argument = base_64_decode(arguments[0][i]);
                        unsigned long outlen = 512;
                        unsigned char *output = new unsigned char[512];
                        int val = 0;
-                       rsa_decrypt_key_ex((unsigned char*)(void*)argument.c_str(),argument.size(),output,&outlen,NULL,NULL,0,LTC_LTC_PKCS_1_V1_5,&val,&key);
+
+                       rsa_decrypt_key_ex((unsigned char*)(void*)argument.c_str(),argument.size(),output,&outlen,NULL,NULL,0,LTC_PKCS_1_V1_5,&val,&key);
                        arguments[0][i] = string(output,output + outlen);
                    }
                }
                if(arguments[0][0] == ":"){
+                    if(arguments[0][1] == "CHANUSERNAMES"){
+                        for(int i = 2; i <arguments[0].size();i++)
+                        {
+                            boost::future<void> subscribe_future = session->subscribe("com.audiodata." + arguments[0][i],&audio_play).then([&] (boost::future<autobahn::wamp_subscription> subscribed)
+                            {
+                                try {
+                                    current_user.channelusers.push_back(RemoteUser(arguments[0][i],subscribed.get()));
+                                    session->subscribe("com.audiodata." + arguments[0][i],&audio_play);
+                                    cout << "Channel user: " << arguments[0][i] << endl;
+                                }
+                                catch (const std::exception& e) {
+                                    std::cerr << e.what() << std::endl;
+                                    io.stop();
+                                    return;
+                            } });
+                        }
+                    }
+                    if(arguments[0][1] == "MESSAGE"){
+                        cout << "<" << arguments[0][2] << ">" << " " << arguments[0][4] << endl;
+                    }
 
+                    if(arguments[0][1] == "CHANNAMES"){
+                        for(int i = 2; i <arguments[0].size();i++)
+                        {
+                            cout << "Channel: " << arguments[0][i] << endl;
+                        }
+                    }
                }
                else{
 
@@ -94,9 +162,18 @@ int main(void)
 {
     ltc_mp = ltm_desc;
     register_prng(&sprng_desc);
+    int err;
+    Pa_Initialize();
+    Pa_OpenDefaultStream(&stream,2,2,paInt16,48000,4,NULL,NULL);
+    Pa_StartStream(stream);
+    encoder = opus_encoder_create(48000,2,OPUS_APPLICATION_AUDIO,&err);
+    decoder = opus_decoder_create(48000,2,&err);
+    err = opus_encoder_ctl(encoder,OPUS_SET_BITRATE(48000));
+
     if (rsa_make_key(NULL, find_prng("sprng"), 1536/8, 65537, &key) != CRYPT_OK) {
         return -1;
     }
+    fclose(stderr);
     unsigned char* public_key = new unsigned char[512];
     memset(public_key,0,512);
     unsigned long length = 512;
@@ -171,6 +248,11 @@ int main(void)
             publish_to_channel("com.audioctl." + current_user.name,vector<string>(1,"QUIT"));
             exit(-1);
         }
+        else if(cmd[0] == "listchannels")
+        {
+            publish_to_channel("com.audioctl." + current_user.name,vector<string>(1,"CHANNAMES"));
+            continue;
+        }
         if(cmd.size() >= 2)
         {
             vector<string> send_to_client;
@@ -181,13 +263,39 @@ int main(void)
                 publish_to_channel("com.audioctl." + current_user.name,send_to_client);
                 continue;
             }
-
-            if(cmd[0] == "joinchannel")
+            else if(cmd[0] == "joinchannel")
             {
+                if(current_user.channel == "")
+                {
                 send_to_client.push_back("JOINCHANNEL");
                 send_to_client.push_back(cmd[1]);
                 current_user.channel = cmd[1];
                 publish_to_channel("com.audioctl." + current_user.name,send_to_client);
+                }
+                else
+                {
+                    cout << "You need to leave your channel first." << endl;
+                }
+                continue;
+            }
+            else if(cmd[0] == "leavechannel")
+            {
+                if(current_user.channel != "")
+                {
+                send_to_client.push_back("LEAVECHANNEL");
+                send_to_client.push_back(cmd[1]);
+                current_user.channel = "";
+                for(RemoteUser user : current_user.channelusers)
+                {
+                    session->unsubscribe(user.subscription);
+                }
+                current_user.channelusers.clear();
+                publish_to_channel("com.audioctl." + current_user.name,send_to_client);
+                }
+                else
+                {
+                    cout << "You need to be in a channel." << endl;
+                }
                 continue;
             }
         }
@@ -196,11 +304,12 @@ int main(void)
     {
         if(current_user.channel != "")
         {
-            //  vector<string> send_to_server;
-            //  send_to_server.push_back("MESSAGE");
-            //  send_to_server.push_back(current_user.channel);
-            //  send_to_server.push_back(command);
-            //  publish_to_channel("com.audioctl.",send_to_server);
+              vector<string> send_to_server;
+              send_to_server.push_back("MESSAGE");
+              send_to_server.push_back(current_user.channel);
+              send_to_server.push_back(command);
+              publish_to_channel("com.audioctl." + current_user.name,send_to_server);
+              continue;
         }
     }
 }
